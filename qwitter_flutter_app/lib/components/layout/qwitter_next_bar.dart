cbe633e1bdaa43097a0002abfd62b56587540b96
OmarMah <36341168+OmarMah@users.noreply.github.com>
2023-10-31 11:46:01 +0200

    Authentication<NextBarWidget>: 	- Added the bottom navigation bar with next button

---
+++ qwitter_flutter_app/lib/components/layout/qwitter_next_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:qwitter_flutter_app/components/basic_widgets/secondary_button.dart';
+
+class QwitterNextBar extends StatelessWidget {
+  const QwitterNextBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 11, horizontal: 5),
+      decoration: const BoxDecoration(
+        color: Colors.black,
+        border: Border(
+          top: BorderSide(
+            color: Colors.grey, // Border color
+            width: 0.05, // Border width
+          ),
+        ),
+      ),
+      child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+        Padding(
+          padding: EdgeInsets.only(right: 16),
+          child: SecondaryButton(text: 'Next', on_pressed: () {}),
+        ),
+      ]),
+    );
+  }
+}
