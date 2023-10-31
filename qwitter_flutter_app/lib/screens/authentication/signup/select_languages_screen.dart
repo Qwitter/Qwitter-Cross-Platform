0e5f25bcdec53dd2664b1f8c1fee3fe35119be54
OmarMah <36341168+OmarMah@users.noreply.github.com>
2023-10-31 12:48:18 +0200

    Authentication<SignUp>: 	- Added the select languages screen

---
+++ qwitter_flutter_app/lib/screens/authentication/signup/select_languages_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:qwitter_flutter_app/components/language_filter.dart';
+import 'package:qwitter_flutter_app/components/layout/qwitter_app_bar.dart';
+import 'package:qwitter_flutter_app/components/layout/qwitter_next_bar.dart';
+
+class SelectLanguagesScreen extends StatelessWidget {
+  const SelectLanguagesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(75),
+        child: QwitterAppBar(),
+      ),
+      body: Container(
+        width: double.infinity,
+        color: Colors.black,
+        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 25),
+        child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Select your language(s)',
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Color.fromARGB(255, 222, 222, 222),
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Select the language(s) you want to use to personalize your X experience.',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Color.fromARGB(255, 132, 132, 132),
+                ),
+              ),
+              SizedBox(height: 5),
+              LanguageFilter(),
+            ]),
+      ),
+      bottomNavigationBar: const QwitterNextBar(),
+    );
+  }
+}
