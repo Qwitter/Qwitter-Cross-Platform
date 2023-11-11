dbaf6b6ef949f4782d9d9247f3b2c7f6c3ccca60
3abqreno <abdelrahman.abdelfatah35@gmail.com>
2023-11-11 17:36:59 +0200

    Messaging <ConversationsScreen>:
    	-added conversationData class
    	-added conversationsWidget

---
+++ qwitter_flutter_app/lib/models/conversation_data.dart
@@ -0,0 +1,14 @@
+class ConversationData {
+  ConversationData({
+    required this.name,
+    required this.handle,
+    required this.lastMsg,
+    required this.lastMsgDate,
+    required this.iconPath,
+  });
+  String name;
+  String handle;
+  String lastMsg;
+  String iconPath;
+  DateTime lastMsgDate;
+}
