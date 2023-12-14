473506e79413579fd965506c36fc39dd5dd67174
Abdelrahman Mohamed Mahmoud Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2023-12-14 15:30:29 +0200

    Messaging (#49)
    * Messaging <ConversationsScreen>:
    
    	-added conversationData class
    	-added conversationsWidget
    
    * Messaging <ConversationsScreen>:
    
    	-added ConversationsScreen
    
    * Messaging <MessagingScreen>:
    
    	-added message class
    	-added messaging text field widget
    	-added scrollable messages widget
    
    * Messaging <MessagingScreen>:
    
    	- added MessagingScreen
    
    * Messaging <conversationUserSearch>:
    
    	-added MessageUserSearchData class
    	-added UserMessageSearchCard widget
    
    * Revert "Merge branch 'main' into messaging"
    
    This reverts commit b44814ee5adfcd6cf51cc6e7215a20fc9a513aeb, reversing
    changes made to 558077c83fca43687547385209a9cd511ad55458.
    
    * Messaging <ConversationScreen/MessagingScreen>:
    
    	-fixed appbar in messagingScreen
    	-changed color scheme to match app (temp fix)
    	-added riverpod optimization to messagingScreen
    	-fixed messages getting scramblled in scrollable_messages
    	class (made sort:false)
    
    * Messaging <NewConverstaionScreen>:
    
    	-added navigation from conversationWidget to MessagingScreen
    	-added searchUserWidget
    	-added user_search_provider
    	-added refresh to conversationScreen
    	-added createConversationScreen
    	-added messagingService
    
    * Messaging<MessagingScreen>:
    
    	-added socket messaging for new messages
    	-added hold on message functionalites
    
    * Messaging<testing>:
    
    	-added create_conversation_test.dart
    	-fixed typo in messaging text field
    
    ---------
    
    Co-authored-by: 3abqreno <abdelrahman.abdelfatah35@gmail.com>
    Co-authored-by: OmarMah <36341168+OmarMah@users.noreply.github.com>

--- qwitter_flutter_app/lib/components/scrollable_messages.dart
+++ qwitter_flutter_app/lib/components/scrollable_messages.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:grouped_list/grouped_list.dart';
 import 'package:intl/intl.dart';
 import 'package:qwitter_flutter_app/models/message_data.dart';
+import 'package:qwitter_flutter_app/providers/messages_provider.dart';
 import 'package:qwitter_flutter_app/theme/theme_constants.dart';
 
-class ScrollableMessages extends StatefulWidget {
+class ScrollableMessages extends ConsumerStatefulWidget {
   ScrollableMessages({
     super.key,
     required this.msgs,
@@ -13,10 +17,12 @@ class ScrollableMessages extends StatefulWidget {
   List<MessageData> msgs;
   final ScrollController scrollController;
   @override
-  State<ScrollableMessages> createState() => _ScrollableMessagesState();
+  ConsumerState<ScrollableMessages> createState() {
+    return _ScrollableMessagesState();
+  }
 }
 
-class _ScrollableMessagesState extends State<ScrollableMessages> {
+class _ScrollableMessagesState extends ConsumerState<ScrollableMessages> {
   @override
   Widget build(context) {
     return Expanded(
@@ -44,20 +50,88 @@ class _ScrollableMessagesState extends State<ScrollableMessages> {
               message.byMe ? Alignment.centerRight : Alignment.centerLeft,
           child: Container(
             constraints: const BoxConstraints(maxWidth: 300),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)),
-              elevation: 0,
-              color:
-                  message.byMe ? Colors.blue : Color.fromARGB(255, 50, 57, 64),
-              child: Padding(
-                padding: const EdgeInsets.all(15),
-                child: Text(
-                  message.text,
-                  style: TextStyle(
-                      color: message.byMe
-                          ? white
-                          : Color.fromARGB(255, 232, 231, 231)),
+            child: InkWell(
+              onLongPress: () {
+                // Fluttertoast.showToast(msg: message.text);
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    backgroundColor: black,
+                    titlePadding: EdgeInsets.all(0),
+                    title: Container(
+                      width: double.infinity,
+                      decoration: const BoxDecoration(color: black),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text(
+                                "Reply",
+                                style: TextStyle(
+                                  color: white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: double.infinity,
+                            child: TextButton(
+                              onPressed: () {
+                                final value = ClipboardData(text: message.text);
+                                Clipboard.setData(value);
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text(
+                                "Copy message text",
+                                style: TextStyle(
+                                  color: white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: double.infinity,
+                            child: TextButton(
+                              onPressed: () {
+                                ref
+                                    .watch(messagesProvider.notifier)
+                                    .DeleteMessage(message);
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text(
+                                "Delete Message",
+                                style: TextStyle(
+                                  color: white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                elevation: 0,
+                color: message.byMe
+                    ? Colors.blue
+                    : Color.fromARGB(255, 50, 57, 64),
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Text(
+                    message.text,
+                    style: TextStyle(
+                        color: message.byMe
+                            ? white
+                            : Color.fromARGB(255, 232, 231, 231)),
+                  ),
                 ),
               ),
             ),
---
+++ qwitter_flutter_app/test/create_conversation_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:qwitter_flutter_app/screens/messaging/messaging_screen.dart';
+import 'package:qwitter_flutter_app/screens/messaging/create_conversation_screen.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+
+void main() {
+  testWidgets('CreateConversationScreen UI Test', (WidgetTester tester) async {
+    // Build our widget and trigger a frame.
+    await tester.pumpWidget(
+      ProviderScope(
+        child: MaterialApp(
+          home: CreateConversationScreen(),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    // Find text elements on the screen
+    final titleText = find.text('Create a conversation');
+
+    // Find the search text field
+    final searchTextfield = find.byType(TextField);
+
+    // Find the "Create" button
+    final createButton = find.text('Create');
+
+    // Verify that the text elements are on the screen
+    expect(titleText, findsOneWidget);
+    expect(searchTextfield, findsOneWidget);
+    expect(createButton, findsOneWidget);
+
+    // Simulate entering text into the search field
+    await tester.enterText(searchTextfield, 'TestUser');
+    await tester.pumpAndSettle();
+
+    // Verify that the selected user is displayed
+    expect(find.text('TestUser'), findsOneWidget);
+  });
+}
