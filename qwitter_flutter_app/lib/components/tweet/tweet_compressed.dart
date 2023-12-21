78c93d8a98340c868a27d10d9eba5c0010af237c
abdallahabouelabbas <40715961+abdallahahmedali@users.noreply.github.com>
2023-12-22 01:07:43 +0200

    Tweets features finalize (#62)
    * Tweets<Avatar>:
    	- Added a Handler to Avatar
    
    * Tweets<Header>:
    	- Added Follow and Unfollow Features
    	- Tweet Header is Responsive now
    
    * Tweets<MediaHandlingExceptions>:
    	- In case of a non valid URL
    
    * Tweets<TweetCard>:
    	- Responsivity of header
    	- Retweet/Undo Retweet
    
    * Tweets<Providers>:
    	- Added Features for For You & Following in Timeline using
    	  separate providers
    
    * Tweets<AddTweetOrReplyCallBack>:
    	- Affecting the timeline and details replies when add or reply
    	  to a tweet
    
    * Tweets<TweetDetails>:
    	- Effect of reply to append reply after insertion
    
    * Tweets<Feed>:
    	- Preview the providers data in feed after separation of For You
    	  and Following section
    
    * Tweets<Services>:
    	- Solving some bugs as AppUser caching
    	- Adding services such as Follow/Unfollow/ForYouFetch
    
    * Notifications<GUI>:
    	- Base GUI
    
    * Tweets<Details>:
    	- Add Refresh Event for Replies refetch

--- qwitter_flutter_app/lib/components/tweet/tweet_compressed.dart
+++ qwitter_flutter_app/lib/components/tweet/tweet_compressed.dart
@@ -1,99 +1,99 @@
-import 'package:flutter/material.dart';
-import 'package:qwitter_flutter_app/components/tweet/tweet_avatar.dart';
-import 'package:qwitter_flutter_app/components/tweet/tweet_header.dart';
-import 'package:intl/intl.dart';
-import 'package:qwitter_flutter_app/models/tweet.dart';
+// import 'package:flutter/material.dart';
+// import 'package:qwitter_flutter_app/components/tweet/tweet_avatar.dart';
+// import 'package:qwitter_flutter_app/components/tweet/tweet_header.dart';
+// import 'package:intl/intl.dart';
+// import 'package:qwitter_flutter_app/models/tweet.dart';
 
-class TweetCompressed extends StatelessWidget {
-  final String tweetText;
-  final List<Media> tweetImgs;
-  final String tweetUserHandle;
-  final String tweetUserName;
-  final bool tweetUserVerified;
-  final String tweetTime;
-  final bool tweetEdited;
-  final String tweet_avatar;
+// class TweetCompressed extends StatelessWidget {
+//   final String tweetText;
+//   final List<Media> tweetImgs;
+//   final String tweetUserHandle;
+//   final String tweetUserName;
+//   final bool tweetUserVerified;
+//   final String tweetTime;
+//   final bool tweetEdited;
+//   final String tweet_avatar;
 
-  const TweetCompressed({
-    Key? key,
-    required this.tweetText,
-    required this.tweetImgs,
-    required this.tweet_avatar,
-    required this.tweetUserHandle,
-    required this.tweetUserName,
-    required this.tweetUserVerified,
-    required this.tweetTime,
-    required this.tweetEdited,
-  }) : super(key: key);
+//   const TweetCompressed({
+//     Key? key,
+//     required this.tweetText,
+//     required this.tweetImgs,
+//     required this.tweet_avatar,
+//     required this.tweetUserHandle,
+//     required this.tweetUserName,
+//     required this.tweetUserVerified,
+//     required this.tweetTime,
+//     required this.tweetEdited,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width - 80,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey[800]!,
-                width: 1,
-              ),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    TweetAvatar(avatar: tweet_avatar, radius: 10, username: tweetUserHandle,),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 2, 0, 0),
-                      child: TweetHeader(
-                        tweetUserHandle: tweetUserHandle,
-                        tweetUserName: tweetUserName,
-                        tweetUserVerified: tweetUserVerified,
-                        tweetTime: tweetTime,
-                        // tweetEdited: tweetEdited,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: Text(
-                  tweetText,
-                  textAlign: Bidi.hasAnyRtl(tweetText)
-                      ? TextAlign.end
-                      : TextAlign.start,
-                  style: TextStyle(
-                    height: 1.2,
-                    fontSize: 16,
-                    color: Colors.white,
-                  ),
-                  softWrap:
-                      true, // Allow text to wrap within the specified width
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              // TweetMedia(
-              //   tweetImgs: tweetImgs,
-              //   radius: 0,
-              // ),
-            ]),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return ClipRRect(
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           SizedBox(
+//             height: 10,
+//           ),
+//           Container(
+//             width: MediaQuery.of(context).size.width - 80,
+//             decoration: BoxDecoration(
+//               border: Border.all(
+//                 color: Colors.grey[800]!,
+//                 width: 1,
+//               ),
+//               borderRadius: BorderRadius.circular(10),
+//             ),
+//             child:
+//                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+//               Padding(
+//                 padding: const EdgeInsets.all(8.0),
+//                 child: Row(
+//                   children: [
+//                     TweetAvatar(avatar: tweet_avatar, radius: 10, username: tweetUserHandle,),
+//                     Padding(
+//                       padding: const EdgeInsets.fromLTRB(0, 2, 0, 0),
+//                       child: TweetHeader(
+//                         tweetUserHandle: tweetUserHandle,
+//                         tweetUserName: tweetUserName,
+//                         tweetUserVerified: tweetUserVerified,
+//                         tweetTime: tweetTime,
+//                         // tweetEdited: tweetEdited,
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 10,
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+//                 child: Text(
+//                   tweetText,
+//                   textAlign: Bidi.hasAnyRtl(tweetText)
+//                       ? TextAlign.end
+//                       : TextAlign.start,
+//                   style: TextStyle(
+//                     height: 1.2,
+//                     fontSize: 16,
+//                     color: Colors.white,
+//                   ),
+//                   softWrap:
+//                       true, // Allow text to wrap within the specified width
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 10,
+//               ),
+//               // TweetMedia(
+//               //   tweetImgs: tweetImgs,
+//               //   radius: 0,
+//               // ),
+//             ]),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
