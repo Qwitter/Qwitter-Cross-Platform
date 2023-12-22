f7340e420196c76b854f8b0820f902295dfb74bb
Aly-Abdel-Motaleb <94588581+Aly-Abdel-Motaleb@users.noreply.github.com>
2023-12-22 19:34:07 +0200

    Searchscreen<HashtagSearchImplemented>: (#63)
    - Added the hashtag search functionality

--- qwitter_flutter_app/lib/components/basic_widgets/custom_search_field.dart
+++ qwitter_flutter_app/lib/components/basic_widgets/custom_search_field.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:qwitter_flutter_app/models/app_user.dart';
 import 'package:qwitter_flutter_app/models/user.dart';
+import 'package:qwitter_flutter_app/providers/hashtag_search_provider.dart';
 import 'package:qwitter_flutter_app/providers/user_profile_search_provider.dart';
 import 'package:qwitter_flutter_app/screens/searching/search_screen.dart';
 
@@ -47,8 +48,37 @@ class _CustomSearchFieldState extends ConsumerState<CustomSearchField> {
       for (var user in result) {
         users.add(User.fromJson(user));
       }
-      print("Iam here");
+      // print("Iam here");
       ref.read(userSearchProfileProvider.notifier).setUsers(users);
+    } else {
+      //print('response status code : ${response.statusCode}');
+      throw Exception('Failed to load user');
+    }
+  }
+
+  void searchHastags(String data) async {
+    final url = Uri.parse(
+        'http://back.qwitter.cloudns.org:3000/api/v1/tweets/hashtags?q=$data');
+
+    final Map<String, String> cookies = {
+      'qwitter_jwt': 'Bearer ${AppUser().getToken}',
+    };
+
+    final response = await http.get(url, headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Cookie': cookies.entries
+          .map((entry) => '${entry.key}=${entry.value}')
+          .join('; '),
+    });
+
+    if (response.statusCode == 200) {
+      List<String> hastags = [];
+      var result = jsonDecode(response.body);
+      for (var hast in result) {
+        hastags.add(hast['text']);
+      }
+      ref.read(hastagSearchProvider.notifier).setHastags(hastags);
     } else {
       //print('response status code : ${response.statusCode}');
       throw Exception('Failed to load user');
@@ -59,23 +89,27 @@ class _CustomSearchFieldState extends ConsumerState<CustomSearchField> {
   Widget build(BuildContext context) {
     return TextFormField(
         controller: widget.controller,
-        onFieldSubmitted: (value){
-          if(value.isEmpty==false){
+        onFieldSubmitted: (value) {
+          if (value.isEmpty == false) {
             Navigator.pop(context);
-            Navigator.push(context, MaterialPageRoute(builder: (context){ return SearchScreen(hastag: "", query: value);}));
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SearchScreen(hastag: "", query: value);
+            }));
           }
         },
         onChanged: (value) {
           if (value.isNotEmpty) {
             setState(() {
               isIconShowed = true;
+              searchUser(value);
+              searchHastags(value);
             });
-            searchUser(value);
           } else {
             setState(() {
               isIconShowed = false;
             });
             ref.read(userSearchProfileProvider.notifier).remove();
+            ref.read(hastagSearchProvider.notifier).remove();
           }
         },
         style: const TextStyle(color: Color.fromARGB(255, 29, 155, 240)),
@@ -91,6 +125,7 @@ class _CustomSearchFieldState extends ConsumerState<CustomSearchField> {
                       isIconShowed = false;
                     });
                     ref.read(userSearchProfileProvider.notifier).remove();
+                    ref.read(hastagSearchProvider.notifier).remove();
                   },
                   icon: const Icon(Icons.clear))
               : const SizedBox(),
