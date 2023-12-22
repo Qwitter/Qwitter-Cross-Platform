18a029715b9612ece5c21bce812920e0aca34f48
Aly-Abdel-Motaleb <94588581+Aly-Abdel-Motaleb@users.noreply.github.com>
2023-12-22 21:06:29 +0200

    Searchscreen <FixSearchBug>
    Fixing the search bug

--- qwitter_flutter_app/lib/components/basic_widgets/custom_search_field.dart
+++ qwitter_flutter_app/lib/components/basic_widgets/custom_search_field.dart
@@ -44,11 +44,12 @@ class _CustomSearchFieldState extends ConsumerState<CustomSearchField> {
       List<User> users = [];
       var result = jsonDecode(response.body);
       result = result['users'];
-
+      
       for (var user in result) {
         users.add(User.fromJson(user));
       }
       // print("Iam here");
+
       ref.read(userSearchProfileProvider.notifier).setUsers(users);
     } else {
       //print('response status code : ${response.statusCode}');
@@ -78,6 +79,7 @@ class _CustomSearchFieldState extends ConsumerState<CustomSearchField> {
       for (var hast in result) {
         hastags.add(hast['text']);
       }
+      
       ref.read(hastagSearchProvider.notifier).setHastags(hastags);
     } else {
       //print('response status code : ${response.statusCode}');
@@ -98,12 +100,15 @@ class _CustomSearchFieldState extends ConsumerState<CustomSearchField> {
           }
         },
         onChanged: (value) {
+          print(value);
           if (value.isNotEmpty) {
             setState(() {
               isIconShowed = true;
-              searchUser(value);
-              searchHastags(value);
             });
+            ref.read(userSearchProfileProvider.notifier).remove();
+            searchUser(value);
+            ref.read(hastagSearchProvider.notifier).remove();
+            searchHastags(value);
           } else {
             setState(() {
               isIconShowed = false;
